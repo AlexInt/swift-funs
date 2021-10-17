b36aca80d93580d80c8cf22457a03a8359fa864d
Jimmy <veniveci@aliyun.com>
2021-10-17 11:02:59 +0800

    Initial Commit

---
+++ Swift-Funs/Swift_FunsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_FunsApp.swift
+//  Swift-Funs
+//
+//  Created by jimmy on 2021/10/17.
+//
+
+import SwiftUI
+
+@main
+struct Swift_FunsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
